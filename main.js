39d4a4cb9c6cd8acf2f50972813a273bf4e3df3c
abduvokhidoff <abcdefgmananam@gmail.com>
2024-09-29 22:46:49 +0500

    doen

---
+++ main.js
@@ -0,0 +1,12 @@
+let num = prompt('Ismingizni kiriting!')
+let num2 = prompt('Space ID kiriting!')
+let num3 = prompt('Parolingizni kiriting!')
+
+
+let obj = {
+    name: num,
+    id: num2,
+    parol: num3
+}
+
+console.log(obj);
